bd5889f94049f37bb36a98d59f61964f01d04379
Jacob Townson <obewanjacobi@aim.com>
2018-06-19 17:13:03 -0400

    did some more work on the project. Really diving into problem 2 now, just need some more time to work on it. Updated .gitignore so that cached material isn't pushed (can be changed if needed). Now only the important parts are pushed.

--- readindata.R
+++ readindata.R
@@ -12,3 +12,6 @@ colnames(prob1_data) = c('Date','oil.flow_barrels','power_megawatt')
 
 prob2_data = read.xlsx("./Draft_Problem_set.2.xlsx", 
                         sheetIndex=2,header=TRUE,startRow=1)
+
+cushing_data = read.xlsx('./cushing.pipedata.xlsx',
+                         sheetIndex=1, header=TRUE, startRow = 2, endRow = 7)
